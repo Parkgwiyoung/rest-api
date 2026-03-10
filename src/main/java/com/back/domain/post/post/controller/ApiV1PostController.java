af7837d0eeba51bdf46d15b4eb5e91eee967bad8
Parkgwiyoung <pgwi11@naver.com>
2026-03-10 15:30:22 +0900

    8 강

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -1,10 +1,10 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,9 +18,14 @@ public class ApiV1PostController {
 
     @GetMapping("/api/v1/posts")
     @ResponseBody
-    @Transactional(readOnly = true)
-    public List<Post> list() {
-        return postService.findAll();
-    }
+    public List<PostDto> list() {
+        List<Post> result = postService.findAll();
 
+        List<PostDto> postDtoList = result.stream()
+                .map(p -> (
+                        new PostDto(p.getId(), p.getTitle(), p.getContent(), p.getCreateDate(), p.getModifyDate())))
+                .toList();
+
+        return postDtoList;
+    }
 }
