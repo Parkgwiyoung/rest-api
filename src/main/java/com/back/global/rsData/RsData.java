8923fc1afcde580a55858765cb0d5c3352dd20d2
Parkgwiyoung <pgwi11@naver.com>
2026-03-12 10:27:23 +0900

    26-2ê 강

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -1,5 +1,7 @@
 package com.back.global.rsData;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public record RsData<T>(
         String msg,
         String resultCode,
@@ -8,6 +10,7 @@ public record RsData<T>(
     public RsData(String msg, String resultCode){
         this(msg, resultCode, null);
     }
+    @JsonIgnore
     public int getStatusCode() {
         return Integer.parseInt(resultCode.split("-")[0]);
     }
